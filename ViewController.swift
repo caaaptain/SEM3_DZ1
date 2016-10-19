3be7f0fa1fd7da22e21929242ac26e53a14cacdb
caaaptain <Ruslan.alexandrovichh@yandex.ru>
2016-10-19 22:03:19 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  BuyNow
+//
+//  Created by RUSLAN ALEXANDROVICH on 07.10.16.
+//  Copyright © 2016 RUSLAN ALEXANDROVICH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+/*
+class Транспорт {
+    var модель : String
+    var объемдвигателя : Double
+    var цвет : String
+    var стоимость : Double
+    
+    init(модель : String , объемдвигателя : Double , цвет : String , стоимость : Double) {
+        self.модель = "BMW"
+        self.объемдвигателя = 3.2
+        self.цвет = "синий"
+        self.стоимость = 3_000_000
+    }
+}
+
+class автомобили:Транспорт{
+    
+}
+
+class мотоциклы:Транспорт{
+    
+}
+
+class запчасти:Транспорт{
+    
+}
+
+class водный_транспорт:Транспорт{
+    
+}
+
+class грузовики_и_спецтехника:Транспорт{
+    
+}
+
+let машина1 = Транспорт(модель: "BMW" , объемдвигателя : 3.4 , цвет : "синий" , стоимость : 3_200_000)
+машина1
+*/
